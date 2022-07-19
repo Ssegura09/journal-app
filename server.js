d660c010bb34ca5ca64161d3e6d89e15969eab91
Stephanie Segura <Steph@Stephanies-MBP.attlocal.net>
2022-07-19 00:52:25 -0500

    boiler plate for basic api mssg on localhost without models

--- server.js
+++ server.js
@@ -1,16 +1,41 @@
 // DEPENDENCIES
 const express = require('express')
 const app = express()
+const { Sequelize } = require('sequelize')
 
 // CONFIGURATION / MIDDLEWARE
 require('dotenv').config()
 app.use(express.json())
 app.use(express.urlencoded({ extended: false }))
 
+const user = "postgres";
+const host = "localhost";
+const database = "journal_app";
+const port = "5432";
+const pass = "postgres";
+
+// SEQUELIZE CONNECTION
+const sequelize = new Sequelize(database, user, pass, {
+    host,
+    port,
+    dialect: "postgres",
+    logging: false
+    
+})
+
+try {
+    sequelize.authenticate() 
+    console.log(`Connected with Sequelize at ${process.env.PG_URI}`) 
+} catch(err) {
+    console.log(`Unable to connect to PG: ${err}`) 
+}
+
+
+
 // ROOT
 app.get('/', (req, res) => {
     res.status(200).json({
-        message: 'Welcome to the Tour API'
+        message: 'Welcome to my journal app'
     })
 })
 
