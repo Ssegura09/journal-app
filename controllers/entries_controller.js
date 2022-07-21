1adfe7c7b4868272b48874c3dc859c5ff282253c
Stephanie Segura <Steph@Stephanies-MBP.attlocal.net>
2022-07-21 13:40:02 -0500

    modified entry controller to show entries in desc order

--- controllers/entries_controller.js
+++ controllers/entries_controller.js
@@ -6,7 +6,10 @@ const { Entry } = db
 // FIND ALL ENTRIES
 entries.get('/', async (req, res) => {
     try {
-        const foundEntries = await Entry.findAll()
+        const foundEntries = await Entry.findAll({
+            order: [ [ 'date', 'DESC']]
+            
+        })
         res.status(200).json(foundEntries)
     } catch (error) {
         res.status(500).json(error)
